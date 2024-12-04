d2cdbe830ee74f48a78a4ef4e5f2f0457a547c13
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-12-04 19:28:09 +0500

    :lipstick: Add global styles

---
+++ src/components/global-styles.tsx
@@ -0,0 +1,82 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+const styles = css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: unset;
+    padding: unset;
+  }
+
+  html {
+    -moz-text-size-adjust: none;
+    -webkit-text-size-adjust: none;
+    text-size-adjust: none;
+  }
+
+  ul[role='list'],
+  ol[role='list'] {
+    list-style: none;
+  }
+
+  body {
+    min-height: 100vh;
+    line-height: 1.5;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  button,
+  input,
+  label {
+    line-height: 1.1;
+  }
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    text-wrap: balance;
+  }
+
+  p {
+    text-wrap: pretty;
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+`;
+
+const GlobalStyles = createGlobalStyle`
+  ${styles}
+`;
+
+export default GlobalStyles;
