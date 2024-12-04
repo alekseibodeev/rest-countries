6ed344fbc1ba45d97c6ab49570521d71b5d884d1
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-12-04 19:38:50 +0500

    :sparkles: Update root route

--- src/app/routes/root.tsx
+++ src/app/routes/root.tsx
@@ -1,3 +1,31 @@
-const Root = () => <h1>Hello!</h1>;
+import { Outlet } from 'react-router';
+import styled from 'styled-components';
+
+import Container from '@/components/container';
+import Header from '@/components/header';
+
+const Wrapper = styled.div`
+  color: ${({ theme }) => theme.color.text};
+  background-color: ${({ theme }) => theme.color.background};
+  font-family: 'Nunito Sans', sans-serif;
+  font-size: 1rem;
+  font-weight: 300;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  min-height: 100vh;
+`;
+
+const Root = () => {
+  return (
+    <Wrapper>
+      <Header />
+      <main>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+    </Wrapper>
+  );
+};
 
 export default Root;
