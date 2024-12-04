7b9483621485f237f84accae6c55381626818717
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-12-04 19:27:31 +0500

    :lipstick: Add color theme

---
+++ src/constants/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  color: {
+    text: 'hsl(200 15% 8%)',
+    background: 'hsl(0 0% 98%)',
+    element: 'hsl(0 0% 100%)',
+    input: 'hsl(0 0% 52%)',
+    shadow: 'hsl(200 15% 8% / 10%)',
+  },
+};
