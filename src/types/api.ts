4bbb01e799389b7190d040c26366dbd5b748d6c4
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-12-04 15:21:43 +0500

    :label: Add api data type

---
+++ src/types/api.ts
@@ -0,0 +1,43 @@
+export type CountryData = {
+  alpha2Code: string;
+  alpha3Code: string;
+  altSpellings: string[];
+  area: number;
+  borders: string[];
+  callingCodes: string[];
+  capital: string[];
+  cioc: string[];
+  currencies: {
+    code: string;
+    name: string;
+    symbol: string;
+  }[];
+  demonym: string;
+  independent: string;
+  flag: string;
+  flags: {
+    svg: string;
+    png: string;
+  };
+  gini?: string;
+  languages: {
+    iso639_1: string;
+    iso639_2: string;
+    name: string;
+    nativeName: string;
+  }[];
+  latlng: [number, number];
+  name: string;
+  nativeName: string;
+  numericCode: string;
+  population: number;
+  region: string;
+  regionalBlocs: {
+    acronym: string;
+    name: string;
+  }[];
+  subregion: string;
+  timezones: string[];
+  topLevelDomain: string[];
+  translations: { [key: string]: string };
+};
