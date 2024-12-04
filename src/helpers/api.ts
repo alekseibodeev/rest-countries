994041bf1fed2317a48fdbcb8816a414f761849e
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-12-04 16:15:29 +0500

    :sparkles: Add api data fetcher

---
+++ src/helpers/api.ts
@@ -0,0 +1,52 @@
+import { CountryData } from '@/types/api';
+
+type QueryParams =
+  | 'alpha3Code'
+  | 'borders'
+  | 'capital'
+  | 'currencies'
+  | 'flag'
+  | 'languages'
+  | 'name'
+  | 'nativeName'
+  | 'population'
+  | 'region'
+  | 'subregion'
+  | 'topLevelDomain';
+
+const API_BASE_URL = 'https://restcountries.com/v2/all';
+
+const QUERY_PARAMS: Array<QueryParams> = [
+  'alpha3Code',
+  'borders',
+  'capital',
+  'currencies',
+  'flag',
+  'languages',
+  'name',
+  'nativeName',
+  'population',
+  'region',
+  'subregion',
+  'topLevelDomain',
+];
+
+const QUERY = `${API_BASE_URL}?fields=${QUERY_PARAMS.join()}`;
+
+let cache: Array<Pick<CountryData, QueryParams>> | undefined;
+
+export const fetchCountries = async () => {
+  if (cache) return cache;
+
+  const res = await fetch(QUERY);
+
+  if (res.status >= 400) {
+    throw new Error(`Error ${res.status}: ${res.statusText}`);
+  }
+
+  const data = (await res.json()) as Array<Pick<CountryData, QueryParams>>;
+
+  cache = data;
+
+  return cache;
+};
