1c18884be62a02ab105ac78fafbc15cde2fe58f5
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-12-07 13:00:40 +0500

    :sparkles: Add a bunch of helper functions

---
+++ src/helpers/util.ts
@@ -0,0 +1,40 @@
+import useCountries from '@/hooks/use-countries';
+
+import { Currency, Language } from '@/types/api';
+
+/**
+ * Replaces all spaces with dashes in the given string and downcase it.
+ */
+export const applyKebab = (str: string) => str.toLowerCase().replace(' ', '-');
+
+/**
+ * Replaces all dashes with spaces in the given string
+ */
+export const removeKebab = (str: string) => str.replace('-', ' ');
+
+export const formatPopulation = (population: number) => {
+  return population.toLocaleString();
+};
+
+export const formatLanguages = (languages: Language[]) => {
+  return languages.map(({ name }) => name).join(', ');
+};
+
+export const formatCurrencies = (currencies: Currency[]) => {
+  return currencies.map(({ name }) => name).join(', ');
+};
+
+export const findBorderCountries = (
+  borders: string[],
+  countries: NonNullable<ReturnType<typeof useCountries>['data']>,
+) => {
+  const bordersSet = new Set(borders);
+  const borderCountries: typeof countries = [];
+
+  for (const country of countries) {
+    if (borderCountries.length === borders.length) return borderCountries;
+    if (bordersSet.has(country.alpha3Code)) borderCountries.push(country);
+  }
+
+  return borderCountries;
+};
